3d600926e584801ee4b6be2ac56a7f8abbbfac1f
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2015-12-07 16:39:01 +0300

    header

---
+++ _header.php
@@ -0,0 +1,23 @@
+<div id="header">
+    <a class="menu-switcher -center" href="#">
+        <img src="images/menu.png" alt="">
+    </a>
+    <a class="logo" href="#">
+        Л<img src="images/logo.png" alt="">катус
+    </a>
+
+    <form class="search">
+        <input class="-box" placeholder="Найти">
+        <button class="-box -green" type="submit"><img src="images/search.png" alt=""></button>
+    </form>
+
+    <a class="coop -right" href="#">
+        Сотрудничество
+    </a>
+    <a class="login -right" href="#">
+        Войти
+    </a>
+    <a class="city -right" href="#">
+        <button class="-green">Москва</button>
+    </a>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Locatus</title>
+
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/-global.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+
+    <script type="text/javascript" src="http://yastatic.net/jquery/2.1.3/jquery.min.js"></script>
+</head>
+
+<body>
+    <? require '_header.php'; ?>
+</body>
+</html>
