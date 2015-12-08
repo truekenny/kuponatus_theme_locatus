12f272ee9ff596d05c77996095489e867055363f
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2015-12-08 19:44:43 +0300

    map

--- index.php
+++ index.php
@@ -13,15 +13,25 @@
     <link rel="stylesheet" type="text/css" href="css/_base.css">
     <link rel="stylesheet" type="text/css" href="css/_base_title.css">
     <link rel="stylesheet" type="text/css" href="css/_base_filter.css">
+    <link rel="stylesheet" type="text/css" href="css/_map.css">
+    <link rel="stylesheet" type="text/css" href="css/adaptive.css">
 
-    <!--<script type="text/javascript" src="http://yastatic.net/jquery/2.1.3/jquery.min.js"></script>-->
-</head>
+    <script type="text/javascript" src="http://yastatic.net/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+    <script type="text/javascript">var Locatus = {};</script>
+    <script src="js/map.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            Locatus.Map.init();
+        });
+    </script></head>
 
-<body>
+<body id="body">
     <? require '_header.php'; ?>
-    <div>
+    <div class="content">
         <? require '_menu.php'; ?>
         <? require '_base.php'; ?>
     </div>
+    <? require '_map.php'; ?>
 </body>
 </html>
---
+++ js/map.js
@@ -0,0 +1,24 @@
+Locatus.Map = (function ($) {
+    var map;
+    ymaps.ready(initMap);
+
+    /**
+     * Инициализирует карту
+     */
+    function initMap() {
+        var mapInit = {
+            center: [55.76, 37.64],
+            zoom: 11,
+            controls: ['zoomControl', 'geolocationControl', 'fullscreenControl']
+        };
+
+        map = new ymaps.Map("map", mapInit);
+    }
+
+    function init() {
+    }
+
+    return {
+        init: init
+    }
+})(jQuery);
