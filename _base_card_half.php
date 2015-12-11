5f4b80b779543f9496b56047eb6eeaff354fc8e9
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2015-12-11 16:08:40 +0300

    card_half

--- _base_card_half.php
+++ _base_card_half.php
@@ -1,1 +1,28 @@
+<div id="card_half">
 
+    <? foreach(range(1, 2)as $i): ?>
+    <div class="card">
+        <div class="image -box" style="background-image: url('images/_DELME_card_half_<?= $i ?>.jpg');">
+
+        </div>
+        <div class="text -box">
+            <h2>Ботинки 6 in Premium Boot</h2>
+            <p>
+                Ботинки Timberland, выполнены в стандартном жёлтом цвете. Подошва у ботинок жёлто-белая, верх ботинок и
+                шнурки более тёмного цвета, чем сами ботинки. У обуви имеется подворот, который можно убрать.
+            </p>
+        </div>
+        <div class="break -box">
+
+        </div>
+        <div class="cost">
+            16 190 руб.
+        </div>
+        <div class="buttons">
+            <a class="-green" href="#">Забронировать</a>
+            <a href="#">Подробнее</a>
+        </div>
+    </div>
+    <? endforeach; ?>
+
+</div>
