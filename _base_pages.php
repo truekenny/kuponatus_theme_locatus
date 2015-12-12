acce34b7815ce1a4830e9c17495e683140a9408b
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2015-12-12 14:58:05 +0300

    base_pages

--- _base_pages.php
+++ _base_pages.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kenny
- * Date: 12.12.15
- * Time: 15:29
- */
+<div id="base_pages">
+    <ul>
+        <!-- Пробельные символы между <li> добавят лишний отступ -->
+        <li><a href="#">Назад</a></li><?
+        foreach(range(1, 6) as $i):
+            ?><li><a href="#"><?= $i ?></a></li><?
+        endforeach;
+        ?>...<?
+        ?><li class="active"><a href="#">345</a></li><?
+        ?><li><a href="#">Далее</a></li>
+    </ul>
+</div>
