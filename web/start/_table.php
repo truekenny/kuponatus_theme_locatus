73be41944813a70edba858366cb356f9031a3cce
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2016-02-03 04:47:20 +0300

    mini

--- web/start/_table.php
+++ web/start/_table.php
@@ -1,8 +1,9 @@
 <div class="table">
     <? foreach (range(1, 15) as $i): ?>
+    <? $demoFilter = "filter: hue-rotate(" . rand(-180, 180) . "deg); -webkit-filter: hue-rotate(" . rand(-180, 180) . "deg);"; ?>
     <div class="cell">
         <div class="head">
-            <h3 style="background-image: url('images/start_cell.png');">
+            <h3 style="background-image: url('images/start_cell.png'); <?= $demoFilter ?>">
                 <a href="#">Медцентры<?= (rand(1, 2) == 1) ? " и ночные рестораны" : ""; ?></a>
             </h3>
         </div>
