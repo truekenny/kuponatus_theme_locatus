42d8e04b06eeea606a785ec1a89acf500fc21140
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2016-02-17 12:52:41 +0300

    Учёт относительных путей для формы поиска

--- web/main/_header.php
+++ web/main/_header.php
@@ -7,7 +7,7 @@
     </a>
 
     <div class="search-wrapper ya-site-form ya-site-form_inited_no"
-         onclick="return {'action':'http://<?= htmlspecialchars($_SERVER["HTTP_HOST"]) ?>/','arrow':false,'bg':'transparent','fontsize':12,'fg':'#000000','language':'ru','logo':'rb','publicname':'Locatus','suggest':true,'target':'_self','tld':'ru','type':3,'usebigdictionary':true,'searchid':2250973,'input_fg':'#000000','input_bg':'#ffffff','input_fontStyle':'normal','input_fontWeight':'normal','input_placeholder':'','input_placeholderColor':'#000000','input_borderColor':'#7f9db9'}">
+         onclick="return {'action':'http://<?= htmlspecialchars($_SERVER["HTTP_HOST"] . dirname($_SERVER["SCRIPT_NAME"])) ?>','arrow':false,'bg':'transparent','fontsize':12,'fg':'#000000','language':'ru','logo':'rb','publicname':'Locatus','suggest':true,'target':'_self','tld':'ru','type':3,'usebigdictionary':true,'searchid':2250973,'input_fg':'#000000','input_bg':'#ffffff','input_fontStyle':'normal','input_fontWeight':'normal','input_placeholder':'','input_placeholderColor':'#000000','input_borderColor':'#7f9db9'}">
         <form class="search -box" action="https://yandex.ru/search/site/" method="get" target="_self">
 
             <!-- Здесь надо вставить ID, который Яндекс выдаёт при регистрации формы -->
--- web/start/_header.php
+++ web/start/_header.php
@@ -18,7 +18,7 @@
     </div>
 
     <div class="search ya-site-form ya-site-form_inited_no"
-         onclick="return {'action':'http://<?= htmlspecialchars($_SERVER["HTTP_HOST"]) ?>/','arrow':false,'bg':'transparent','fontsize':12,'fg':'#000000','language':'ru','logo':'rb','publicname':'Locatus','suggest':true,'target':'_self','tld':'ru','type':3,'usebigdictionary':true,'searchid':2250973,'input_fg':'#000000','input_bg':'#ffffff','input_fontStyle':'normal','input_fontWeight':'normal','input_placeholder':'','input_placeholderColor':'#000000','input_borderColor':'#7f9db9'}">
+         onclick="return {'action':'http://<?= htmlspecialchars($_SERVER["HTTP_HOST"] . dirname($_SERVER["SCRIPT_NAME"])) ?>','arrow':false,'bg':'transparent','fontsize':12,'fg':'#000000','language':'ru','logo':'rb','publicname':'Locatus','suggest':true,'target':'_self','tld':'ru','type':3,'usebigdictionary':true,'searchid':2250973,'input_fg':'#000000','input_bg':'#ffffff','input_fontStyle':'normal','input_fontWeight':'normal','input_placeholder':'','input_placeholderColor':'#000000','input_borderColor':'#7f9db9'}">
         <form action="https://yandex.ru/search/site/" method="get" target="_self">
             <input type="hidden" name="searchid" value="2250973"/>
             <input type="hidden" name="l10n" value="ru"/>
