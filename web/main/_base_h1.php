bd43cd6d66ab6a1ed325783d2b5ca70cb09ab933
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2016-05-05 19:26:21 +0300

    Замена h1 на span (SEO)
    p12
    Ref T769#18

--- web/main/_base_h1.php
+++ web/main/_base_h1.php
@@ -1,3 +1,5 @@
 <div class="base-h1">
-    <h1><?= isset($text) ? $text : 'Вот что мы нашли' ?></h1>
+    <noindex>
+        <span class="h1"><?= isset($text) ? $text : 'Вот что мы нашли' ?></span>
+    </noindex>
 </div>
